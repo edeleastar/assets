5d180f38e233043825ad685eee3d42bf994acd32
Eamonn de Leastar <edeleastar@gmail.com>
2014-04-05 17:12:58 +0100

    another attempt to get path correct

--- style/custom/bookmain.js
+++ style/custom/bookmain.js
@@ -1,4 +1,4 @@
 $(document).ready(function()
 {  
-  $(".moodle-book pre").css('background-image', 'url(http://edeleastar.github.org/assets/style/highlight/school_book.png)');
+  $(".moodle-book pre").css('background-image', 'url(http://edeleastar.github.io/assets/style/highlight/school_book.png)');
 });
