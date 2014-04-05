d555dad7ad2ab87d837a3c388c0748db148a9ee6
Eamonn de Leastar <edeleastar@gmail.com>
2014-04-05 17:10:43 +0100

    fix path in js for code block

--- style/custom/bookmain.js
+++ style/custom/bookmain.js
@@ -1,4 +1,4 @@
 $(document).ready(function()
 {  
-  $(".moodle-book pre").css('background-image', 'url(http://edeleastar.github.org/assets/highlight/school_book.png)');
+  $(".moodle-book pre").css('background-image', 'url(http://edeleastar.github.org/assets/style/highlight/school_book.png)');
 });
